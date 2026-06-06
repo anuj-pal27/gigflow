c4b456207580c5659b72dc8afa72686165bacdcb
anuj-pal27 <anujpal27669@gmail.com>
2026-06-06 16:04:27 +0530

    added containerization

--- backend/index.js
+++ backend/index.js
@@ -40,6 +40,12 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
+// Request logger middleware
+app.use((req, res, next) => {
+  console.log(`[${new Date().toLocaleTimeString()}] ${req.method} ${req.url}`);
+  next();
+});
+
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/gigs", gigRoutes);
